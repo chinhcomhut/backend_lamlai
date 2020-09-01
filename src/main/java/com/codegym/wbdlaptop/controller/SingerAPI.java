8e1f54aeb42c03fefd9b16fe6dd13b549d57ea85
chinhcomhut <chinhcomhut@gmail.com>
2020-09-01 12:48:17 +0700

    CRUD singer

--- src/main/java/com/codegym/wbdlaptop/controller/SingerAPI.java
+++ src/main/java/com/codegym/wbdlaptop/controller/SingerAPI.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -50,5 +51,45 @@ public class SingerAPI {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(singers, HttpStatus.OK);
+    }
+    @GetMapping("/singer/{id}")
+    public ResponseEntity<?> singerById(@PathVariable Long id){
+        Optional<Singer> singer = singerService.findById(id);
+        if(!singer.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(singer, HttpStatus.OK);
+    }
+    @DeleteMapping("/singer/{id}")
+    public ResponseEntity<?> deleteSinger(@PathVariable Long id){
+        Optional<Singer> singer = singerService.findById(id);
+        if(!singer.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        singerService.delete(id);
+        return new ResponseEntity<>(new ResponseMessage("yes"), HttpStatus.OK);
+    }
+    @PutMapping("/singer/{id}")
+    public ResponseEntity<?> updateSinger(@PathVariable Long id, @Valid @RequestBody Singer singer){
+        Optional<Singer> singer1 = singerService.findById(id);
+        if(!singer1.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if(singer.getNameSinger()==null||singer.getNameSinger()==""){
+            return new ResponseEntity<>(new ResponseMessage("noname"), HttpStatus.OK);
+        }
+        if(singer.getInformation()==null||singer.getInformation()==""){
+            return new ResponseEntity<>(new ResponseMessage("noinformation"), HttpStatus.OK);
+        }
+        if(singerService.existsByNameSinger(singer.getNameSinger())){
+            return new ResponseEntity<>(new ResponseMessage("nosinger"), HttpStatus.OK);
+        }
+        singer1.get().setInformation(singer.getInformation());
+        singer1.get().setGender(singer.getGender());
+        singer1.get().setBirthday(singer.getBirthday());
+        singer1.get().setAvatarSinger(singer.getAvatarSinger());
+        singer1.get().setNameSinger(singer.getNameSinger());
+        singerService.save(singer1.get());
+        return new ResponseEntity<>(new ResponseMessage("yes"), HttpStatus.OK);
     }
 }
