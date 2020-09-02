c801190853b4dcaa029ec0b4ad2554818ca6c3a3
chinhcomhut <chinhcomhut@gmail.com>
2020-09-03 03:40:41 +0700

    write only

--- src/main/java/com/codegym/wbdlaptop/model/Playlist.java
+++ src/main/java/com/codegym/wbdlaptop/model/Playlist.java
@@ -21,7 +21,7 @@ public class Playlist {
 
     @ManyToOne
     User user;
-    @JsonProperty(access = JsonProperty.Access.AUTO)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @OneToMany
     @JoinTable(name = "player_song",
     joinColumns = @JoinColumn(name = "playlist_id"),
